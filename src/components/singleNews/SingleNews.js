df0683ecfed93b72341388936029692fd0c97bc5
put your github username here <YOUR EMAIL ADDRESS>
2023-04-16 23:14:46 +0000

    made singlenews component

---
+++ src/components/singleNews/SingleNews.js
@@ -0,0 +1,23 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import ModalNews from '../modalNews/ModalNews';
+import { useState } from 'react';
+export default function SingleNews(props){
+    const [showModal, setShowModal] = useState(false);
+    const handleCloseModal = () => setShowModal(false);
+    const handleShowModal = () => setShowModal(true);
+    return (
+        <div id="div">
+        <Card id = 'card' style={{ width: '18rem' }}>
+        <Card.Img style={{height :"400px"}} variant="top" src = {props.data.image} />
+        <Card.Body id="card-body">
+            <Card.Title>{props.data.title}</Card.Title>
+            <div id = 'button'>
+            <Button id="add-fav-btn" variant="primary" onClick={handleShowModal}>Show Details</Button>
+            </div>
+        </Card.Body>
+        </Card>
+        <ModalNews data = {props.data} handleClose = {handleCloseModal} show = {showModal} addCommentProp = {props.addCommentProp}/>
+        </div>
+    )
+}
