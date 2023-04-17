e68fe5e6d030861ffe18a5fa35b6f0777827fe9f
leenalzaben <leena.zaben61@gmail.com>
2023-04-17 08:59:12 +0000

    Leena

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,26 +1,28 @@
 import { useEffect, useState } from "react";
 import AllNews from '../allNews/AllNews';
 
-export default function Home(){
+export default function Home() {
     const url = process.env.REACT_APP_URL;
     const [readLater, setReadLater] = useState([]);
-    async function fetchTrending(){
+    async function fetchTrending() {
         const response = await fetch(`${url}/getNews`);
         const trendingData = await response.json();
         setReadLater(trendingData);
         console.log(readLater);
     }
-    useEffect(() => {fetchTrending()}, []);
-    function addCommentProp(comment, id){
-       for (const movie of readLater) {
-             if (movie.id === id) {
-                 movie.comment = comment;
+    useEffect(() => { 
+        fetchTrending() }, []);
+        
+    function addCommentProp(comment, id) {
+        for (const movie of readLater) {
+            if (movie.id === id) {
+                movie.comment = comment;
             }
         }
     }
     return (
         <>
-        <AllNews data = {readLater} addCommentProp = {addCommentProp}/>
+            <AllNews data={readLater} addCommentProp={addCommentProp} />
         </>
     )
 }
