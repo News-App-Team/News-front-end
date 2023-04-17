35cf3741632ba1e5a657c1831ee02923a3cfd3d5
AbdToughoj <toughoj1@gmail.com>
2023-04-17 22:51:12 +0000

    home page style

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,16 +1,141 @@
-
 import "./Footer.css";
+import { FaFacebook, FaTwitter } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer>
-      <p>&copy; All Right reserved.</p>
-      <div>
-        <a href="https://www.facebook.com" class="fa fa-facebook"></a>
-        <a href="https://www.instagram.com" class="fa fa-instagram"></a>
-        <a href="https://www.whatsapp.com" class="fa fa-whatsapp"></a>
-      </div>
-    </footer>
+    <div className="container-fluid my-5 mx-0 px-0 w-100 ">
+      <footer
+        className="text-center text-lg-start text-white"
+        style={{ "background-color": "#929fbf" }}
+      >
+        <div className="container-fluid p-4 pb-0">
+          <section className="">
+            <div className="row">
+              <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h6 className="text-uppercase mb-4 font-weight-bold">
+                  Company name
+                </h6>
+                <p>
+                  Here you can use rows and columns to organize your footer
+                  content. Lorem ipsum dolor sit amet, consectetur adipisicing
+                  elit.
+                </p>
+              </div>
+
+              <hr className="w-100 clearfix d-md-none" />
+
+              <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h6 className="text-uppercase mb-4 font-weight-bold">
+                  Products
+                </h6>
+                <p>
+                  <a className="text-white">MDBootstrap</a>
+                </p>
+                <p>
+                  <a className="text-white">MDWordPress</a>
+                </p>
+                <p>
+                  <a className="text-white">BrandFlow</a>
+                </p>
+                <p>
+                  <a className="text-white">Bootstrap Angular</a>
+                </p>
+              </div>
+
+              <hr className="w-100 clearfix d-md-none" />
+
+              <hr className="w-100 clearfix d-md-none" />
+
+              <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h6 className="text-uppercase mb-4 font-weight-bold">
+                  Contact
+                </h6>
+                <p>
+                  <i className="fas fa-home mr-3"></i> New York, NY 10012, US
+                </p>
+                <p>
+                  <i className="fas fa-envelope mr-3"></i> info@gmail.com
+                </p>
+                <p>
+                  <i className="fas fa-phone mr-3"></i> + 01 234 567 88
+                </p>
+                <p>
+                  <i className="fas fa-print mr-3"></i> + 01 234 567 89
+                </p>
+              </div>
+
+              <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h6 className="text-uppercase mb-4 font-weight-bold">
+                  Follow us
+                </h6>
+
+                <a
+                  className="btn btn-primary btn-floating m-1"
+                  style={{ "background-color": "#3b5998" }}
+                  href="#!"
+                  role="button"
+                >
+                  <FaFacebook />
+                </a>
+
+                <a
+                  className="btn btn-primary btn-floating m-1"
+                  style={{ "background-color": "#55acee" }}
+                  href="#!"
+                  role="button"
+                >
+                  <FaTwitter />
+                </a>
+
+                <a
+                  className="btn btn-primary btn-floating m-1"
+                  style={{ "background-color": "#dd4b39" }}
+                  href="#!"
+                  role="button"
+                >
+                  <i className="fab fa-google"></i>
+                </a>
+
+                <a
+                  className="btn btn-primary btn-floating m-1"
+                  style={{ "background-color": "#ac2bac" }}
+                  href="#!"
+                  role="button"
+                >
+                  <i className="fab fa-instagram"></i>
+                </a>
+
+                <a
+                  className="btn btn-primary btn-floating m-1"
+                  style={{ "background-color": "#0082ca" }}
+                  href="#!"
+                  role="button"
+                >
+                  <i className="fab fa-linkedin-in"></i>
+                </a>
+                <a
+                  className="btn btn-primary btn-floating m-1"
+                  style={{ "background-color": "#333333" }}
+                  href="#!"
+                  role="button"
+                >
+                  <i className="fab fa-github"></i>
+                </a>
+              </div>
+            </div>
+          </section>
+        </div>
+
+        <div
+          className="text-center p-3"
+          style={{ "background-color": "rgba(0, 0, 0, 0.2)" }}
+        >
+          © 2020 Copyright:
+          <a className="text-white" href="https://mdbootstrap.com/">
+            MDBootstrap.com
+          </a>
+        </div>
+      </footer>
+    </div>
   );
 }
-
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,33 +1,107 @@
 import { useEffect, useState } from "react";
-import AllNews from '../allNews/AllNews';
+import Carousel from "react-bootstrap/Carousel";
+import AllNews from "../allNews/AllNews";
+import "./Home.css";
+import React from "react";
+import {
+  MDBCard,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardBody,
+  MDBCardImage,
+  MDBRow,
+  MDBCol,
+} from "mdb-react-ui-kit";
 
-export default function Home(){
-    
-    const url = process.env.REACT_APP_URL;
+export default function Home() {
+  const url = process.env.REACT_APP_URL;
 
-    const [readLater, setReadLater] = useState([]);
+  const [readLater, setReadLater] = useState([]);
 
-    async function fetchTrending(){
+  async function fetchTrending() {
+    const response = await fetch(`${url}/getNews`);
+    const trendingData = await response.json();
+    setReadLater(trendingData);
+    console.log(readLater);
+  }
 
-        const response = await fetch(`${url}/getNews`);
-        const trendingData = await response.json();
-        setReadLater(trendingData);
-        console.log(readLater);
+  useEffect(() => {
+    fetchTrending();
+  }, []);
+
+  function addCommentProp(comment, id) {
+    for (const movie of readLater) {
+      if (movie.id === id) {
+        movie.comment = comment;
+      }
     }
+  }
 
-    useEffect(() => {fetchTrending()}, []);
+  return (
+    <>
+      <AllNews data={readLater} addCommentProp={addCommentProp} />
+      <Carousel>
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100 carouselImg"
+            src="https://img.freepik.com/free-photo/old-black-background-grunge-texture-dark-wallpaper-blackboard-chalkboard-room-wall_1258-28312.jpg?w=1380&t=st=1681756510~exp=1681757110~hmac=65cadace936c245a441e2afe6b7bfbd405650b3783583a469b52831b52f52957"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={500}>
+          <img
+            className="d-block w-100 carouselImg"
+            src="https://img.freepik.com/free-photo/old-black-background-grunge-texture-dark-wallpaper-blackboard-chalkboard-room-wall_1258-28312.jpg?w=1380&t=st=1681756510~exp=1681757110~hmac=65cadace936c245a441e2afe6b7bfbd405650b3783583a469b52831b52f52957"
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 carouselImg"
+            src="https://img.freepik.com/free-photo/old-black-background-grunge-texture-dark-wallpaper-blackboard-chalkboard-room-wall_1258-28312.jpg?w=1380&t=st=1681756510~exp=1681757110~hmac=65cadace936c245a441e2afe6b7bfbd405650b3783583a469b52831b52f52957"
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
 
-    function addCommentProp(comment, id){
-       for (const movie of readLater) {
-             if (movie.id === id) { 
-                 movie.comment = comment;
-            }
-        }
-    }
-    
-    return (
-        <>
-        <AllNews data = {readLater} addCommentProp = {addCommentProp}/>
-        </>
-    )
+      <MDBCard style={{ maxWidth: "540px" }}>
+        <MDBRow className="g-0">
+          <MDBCol md="4">
+            <MDBCardImage
+              src="https://img.freepik.com/free-photo/old-black-background-grunge-texture-dark-wallpaper-blackboard-chalkboard-room-wall_1258-28312.jpg?w=1380&t=st=1681756510~exp=1681757110~hmac=65cadace936c245a441e2afe6b7bfbd405650b3783583a469b52831b52f52957"
+              alt="..."
+              fluid
+            />
+          </MDBCol>
+          <MDBCol md="8">
+            <MDBCardBody>
+              <MDBCardTitle>Card title</MDBCardTitle>
+              <MDBCardText>
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </MDBCardText>
+              <MDBCardText>
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </MDBCardText>
+            </MDBCardBody>
+          </MDBCol>
+        </MDBRow>
+      </MDBCard>
+    </>
+  );
 }
